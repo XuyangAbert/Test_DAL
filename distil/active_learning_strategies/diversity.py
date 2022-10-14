4fc1c452e8b2d0f9b1d4e78c93870207690549b2
XuyangAbert <53911455+XuyangAbert@users.noreply.github.com>
2022-10-14 15:56:29 -0400

    Add files via upload

--- distil/active_learning_strategies/diversity.py
+++ distil/active_learning_strategies/diversity.py
@@ -8,12 +8,14 @@ Created on Wed Oct  5 11:53:01 2022
 import torch
 from math import exp
 import numpy as np
-from torch.utils.data import dataset
+
+from torch.utils.data import dataset, Subset
 from .strategy import Strategy
 from .margin_sampling import MarginSampling
 class Diversity(Strategy):
   def __init__(self, labeled_dataset, unlabeled_dataset, net, nclass, args={}):
 
+    self.strategy = MarginSampling(labeled_dataset,unlabeled_dataset,net,nclass, args)
     super(Diversity,self).__init__(labeled_dataset, unlabeled_dataset, net, nclass, args)
 
   def dist_cal(self,unlabeled_embeddings):
@@ -38,19 +40,27 @@ class Diversity(Strategy):
     for i in range(interd.shape[0]):
       priority.append(torch.sum(torch.exp(interd[i][:])))
     return torch.tensor(priority)
+  
+  def acquire_scores2(self,unlabeled_batch):
+    
+    scores = self.strategy.acquire_scores(unlabeled_batch)
+    return scores
 
   def select(self, fetchsize):
     embedding_unlabeled = self.get_embedding(self.unlabeled_dataset)
+    
     bs = 10000
     idx = []
     nb = round(embedding_unlabeled.shape[0]/bs)
     for b in range(nb):
       embedding_unlabeled_batch = embedding_unlabeled[b*bs:(b+1)*bs][:]
+      buffered_stream = Subset(self.unlabeled_dataset,list(range(bs)))
       interd = self.dist_cal(embedding_unlabeled_batch)
       dth = self.knei_dist(interd, round(fetchsize/nb))
       # print(dth)
-      priority = self.acquire_scores(interd)
-      # print(priority)
+      # priority = self.acquire_scores(interd)
+      priority = self.acquire_scores2(buffered_stream)
+      # print(len(priority))
       for i in range(round(fetchsize/nb)):
         top_idx = torch.argmax(priority).item()
         idx.append(top_idx)
